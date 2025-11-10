6b845dfb5783a9632de562c7daf813e968772d47
Mohamed Hany Ahmed <mohamedharidy12123@gmail.com>
2025-11-10 22:08:51 +0200

    Update kheap.h

--- kern/mem/kheap.h
+++ kern/mem/kheap.h
@@ -18,9 +18,9 @@
 #define KHP_PLACE_CUSTOMFIT 0x5
 
 //TODO: [PROJECT'25.GM#2] KERNEL HEAP - #0 Page Alloc Limits [GIVEN]
-uint32 kheapPageAllocStart ;
-uint32 kheapPageAllocBreak ;
-uint32 kheapPlacementStrategy;
+extern uint32 kheapPageAllocStart;
+extern uint32 kheapPageAllocBreak;
+extern uint32 kheapPlacementStrategy;
 
 /*2025*/ //Replaced by setter & getter function
 static inline void set_kheap_strategy(uint32 strategy){kheapPlacementStrategy = strategy;}
@@ -31,12 +31,12 @@ void kheap_init();
 
 void* kmalloc(unsigned int size);
 void kfree(void* virtual_address);
-void *krealloc(void *virtual_address, unsigned int new_size);
+void *krealloc(void *virtual_address, uint32 new_size);
 
 unsigned int kheap_virtual_address(unsigned int physical_address);
 unsigned int kheap_physical_address(unsigned int virtual_address);
 
-int numOfKheapVACalls ;
+extern int numOfKheapVACalls;
 
 
 #endif // FOS_KERN_KHEAP_H_
